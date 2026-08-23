abdf5bed23b8d07f4b1b545b53f2e4e1dfd4f76e
tck199732 <ctb8135@wmich.edu>
2026-08-23 15:28:39 -0400

    duplicate and modify trainer and model which specifically handles data with batch tensor inside model class instead of packing/unpacking in trainer class

---
+++ pytorch_src/models/hit_oc_batch.py
@@ -0,0 +1,125 @@
+import torch
+from torch import nn
+
+from base.model import BaseModel
+from layers.attention import FullAttention, AttentionLayer
+from layers.encoders import Encoder, VanillaEncoderLayer
+from layers.oc import ObjectCondensation
+from layers.gnn import GravNet
+from utils.masks import cross_graph_mask
+
+
+
+class BatchHitObjectCondensationModel(BaseModel):
+    """
+    Object Condensation model for hit-level data.
+    """
+
+    def __init__(self, **kwargs):
+        super(BatchHitObjectCondensationModel, self).__init__()
+
+        self.pos_dim = kwargs.get('pos_dim', 2)  # dim of detector position (x,y)
+
+        # gravnet parameters
+        self.n_gravnet_layers = kwargs.get('n_gravnet_layers', 2)
+        self.gravnet_space_dimensions = kwargs.get('gravnet_space_dimensions', 4)
+        self.gravnet_propagate_dimensions = kwargs.get('gravnet_propagate_dimensions', 22)
+        self.gravnet_k = kwargs.get('gravnet_k', 8)
+
+        # node level encoder parameters
+        self.d_model = kwargs.get('d_model', 32)
+        self.n_enc_layers = kwargs.get('n_enc_layers', 2)
+        self.num_heads = kwargs.get('num_heads', 4)
+        self.attn_dropout = kwargs.get('attn_dropout', 0.1)
+        self.attn_ff = kwargs.get('attn_ff', self.d_model * 4)
+
+        # oc mlp parameters
+        self.oc_mlp_pos_nlayers = kwargs.get('oc_mlp_pos_nlayers', 2)
+        self.oc_mlp_pos_hidden = kwargs.get('oc_mlp_pos_hidden', 64)
+        self.oc_mlp_dropout = kwargs.get('oc_mlp_dropout', 0.1)
+        self.oc_mlp_beta_nlayers = kwargs.get('oc_mlp_beta_nlayers', 2)
+        self.oc_mlp_beta_hidden = kwargs.get('oc_mlp_beta_hidden', 64)
+
+        ################################################################################
+        # Encoder for input features
+        ################################################################################
+        self.fea_encoder = nn.Linear(in_features=2, out_features=self.d_model)  # e, t
+        self.geo_mlp = nn.Linear(in_features=2, out_features=self.d_model)  # x, y
+        ################################################################################
+        # Attentional encoder layers
+        ################################################################################
+        gravnet_layers = [
+            GravNet(
+                in_channels=self.d_model if i > 0 else 2,
+                out_channels=self.d_model,
+                space_dimensions=self.gravnet_space_dimensions,
+                propagate_dimensions=self.gravnet_propagate_dimensions,
+                k=self.gravnet_k,
+            )
+            for i in range(self.n_gravnet_layers)
+        ]
+
+        self.geo_embed = nn.ModuleList(gravnet_layers)
+
+        attn_layers = [
+            VanillaEncoderLayer(
+                attn_layer=AttentionLayer(
+                    FullAttention(
+                        mask_flag=True,
+                        attention_dropout=self.attn_dropout,
+                        scale=None,
+                    ),
+                    d_model=self.d_model,
+                    n_heads=self.num_heads,
+                ),
+                d_model=self.d_model,
+                ff_kwargs={
+                    "d_ff": self.attn_ff,
+                    "activation": nn.LeakyReLU,
+                },
+                dropout=self.attn_dropout,
+                batchnorm=False,
+            )
+            for _ in range(self.n_enc_layers)
+        ]
+        self.attn_enc = Encoder(attn_layers)
+
+        self.oc = ObjectCondensation(
+            n_x_layers=self.oc_mlp_pos_nlayers,
+            x_in=self.d_model,
+            x_hidden=self.oc_mlp_pos_hidden,
+            x_out=self.pos_dim,
+            n_beta_layers=self.oc_mlp_beta_nlayers,
+            beta_hidden=self.oc_mlp_beta_hidden,
+            x_dropout=self.oc_mlp_dropout,
+        )
+
+    def forward(
+        self, x: torch.Tensor, pos: torch.Tensor, batch: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input features of shape (num_nodes, 2) representing (energy, time)
+        pos : torch.Tensor
+            Geometric positions of shape (num_nodes, 2) representing (x, y)
+        batch : torch.Tensor
+            Batch vector of shape (num_nodes,) indicating the graph index for each node.
+        """
+
+        x = self.fea_encoder(x)  # [N, d_model]
+
+        pos = self.geo_mlp(pos)  # [N, d_model]
+        for gl in range(self.n_gravnet_layers):
+            pos = self.geo_embed[gl](pos, batch=batch)  # [N, d_model]
+        x = x + pos  # [N, d_model]
+        x = x.unsqueeze(0)  # [1, N, d_model]
+
+        attn_mask = cross_graph_mask(batch)  # [1, 1, N, N]
+
+        x, _ = self.attn_enc(x, attn_mask=attn_mask)  # [1, N, d_model]
+        x = x.squeeze(0)  # [N, d_model]
+
+        x_c, beta = self.oc(x)  # [N, pos_dim], [N, 1]
+        return x_c, beta
---
+++ pytorch_src/training/batch_hit_oc_trainer.py
@@ -0,0 +1,317 @@
+import logging
+import torch
+import pathlib
+from typing import Optional, Tuple
+from torch.export import Dim
+
+from base.trainer import BaseTrainer
+from base.model import BaseModel
+from base.dataloader import BaseDataLoader
+from models.oc_loss import oc_loss_per_batch
+from utils.graph import create_unique_object_ids
+
+
+def create_sample_mask(
+    object_ids: torch.Tensor,
+    batch: Optional[torch.LongTensor] = None,
+    scale: float = 5.0,
+    bkg_id: int = 0,
+) -> torch.Tensor:
+    """
+    Create a mask to only retain a subset of background nodes for training.
+
+    Parameters
+    ----------
+    object_ids: torch.Tensor
+        Tensor of shape (N,) containing object IDs for each node. Background nodes should have the ID equal to bkg_id.
+    batch: Optional[torch.LongTensor]
+        Tensor of shape (N,) indicating the graph index for each node in a batched setting. If None, all nodes are considered to belong to a single graph.
+    scale: float
+        Scaling factor to determine how many background nodes to keep relative to the number of signal nodes.
+    bkg_id: int
+        The object ID that indicates background nodes.
+
+    Returns
+    -------
+    torch.Tensor
+        A boolean mask tensor of shape (N,) where True indicates the node is kept for training.
+    """
+    device = object_ids.device
+    x_size = object_ids.size(0)
+    if batch is None:
+        batch = torch.zeros(x_size, dtype=torch.long, device=device)
+
+    is_bkg = object_ids == bkg_id
+    is_sig = ~is_bkg
+    mask = torch.where(is_sig, True, False)
+
+    for b in batch.unique(sorted=True):
+        node_mask = batch == b
+        nb_sig = (is_sig & node_mask).sum().item()
+        bkg_indices = torch.nonzero(is_bkg & node_mask, as_tuple=False).flatten()
+
+        nb_keep = int((nb_sig + 1) * scale)
+        nb_keep = min(nb_keep, bkg_indices.numel())
+
+        if nb_keep > 0:
+            perm = torch.randperm(bkg_indices.size(0), device=device)
+            selected = bkg_indices[perm[:nb_keep]]
+            mask[selected] = True
+
+    return mask
+
+
+def apply_mask(mask, *tensors):
+    return [t[mask] for t in tensors]
+
+
+class ObjectCondensationTrainer(BaseTrainer):
+    """
+    Base Object Condensation Trainer class. Implements the common training loop and loss computation for object condensation, while allowing for flexible input feature formats and pre-processing steps through methods that can be overridden by subclasses.
+    """
+
+    def __init__(
+        self,
+        model: BaseModel,
+        optimizer: torch.optim.Optimizer,
+        config: dict,
+        device: torch.device,
+        dataloader: BaseDataLoader,
+        valid_dataloader: Optional[BaseDataLoader] = None,
+        lr_scheduler: Optional[torch.optim.lr_scheduler.LRScheduler] = None,
+        logger: Optional[logging.Logger] = None,
+    ):
+        super().__init__(model, optimizer, config, logger)
+
+        self.dataloader = dataloader
+        self.valid_dataloader = valid_dataloader
+        self.lr_scheduler = lr_scheduler
+        self.device = device
+        self.do_validation = self.valid_dataloader is not None
+
+    def _progress(self, batch_idx):
+        """Return a string progress indicator for the current batch index."""
+        base = '[{}/{} ({:.0f}%)]'
+        total = len(self.dataloader)
+        return base.format(batch_idx, total, 100.0 * batch_idx / total)
+
+    def _compute_oc_losses(
+        self,
+        x_c: torch.Tensor,
+        beta: torch.Tensor,
+        object_ids: torch.Tensor,
+        batch: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Compute the object condensation losses for a batch of data. This includes the attractive loss, repulsive loss, cowardly loss, and noise loss.
+
+        Parameters
+        ----------
+        x_c : torch.Tensor
+            The condensed coordinates output by the model, shape [N, C].
+        beta : torch.Tensor
+            The beta values output by the model, shape [N].
+        object_ids : torch.Tensor
+            Tensor of shape [N] containing the object ID for each node.
+        batch : torch.Tensor
+            Tensor of shape [N] indicating the graph index for each node in a batched setting.
+
+        Returns
+        -------
+        Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
+            A tuple containing the attractive loss, repulsive loss, cowardly loss, and noise loss for the batch.
+        """
+        q_min = self.config.get("q_min", 0.3)
+        noise_idx = self.config.get("noise_idx", -1)
+        margin = self.config.get("margin", 1.0)
+
+        attr_scale = self.config.get("attr_scale", 1.0)
+        repul_scale = self.config.get("repul_scale", 1.0)
+        coward_scale = self.config.get("coward_scale", 1.0)
+        noise_scale = self.config.get("noise_scale", 0.0)
+
+        l_attr, l_repul, l_coward, l_noise = oc_loss_per_batch(
+            x=x_c,
+            beta=beta,
+            object_id=object_ids,
+            batch=batch,
+            q_min=q_min,
+            noise_idx=noise_idx,
+            margin=margin,
+        )
+
+        l_attr *= attr_scale
+        l_repul *= repul_scale
+        l_coward *= coward_scale
+        l_noise *= noise_scale
+
+        return l_attr, l_repul, l_coward, l_noise
+
+    def _train_epoch(self, epoch):
+
+        self.model.train()
+        total_loss = 0.0
+
+        noise_idx = self.config.get("noise_idx", -1)
+        downsample = self.config.get("apply_downsample", False)
+        for batch_idx, data in enumerate(self.dataloader):
+
+            self.optimizer.zero_grad()
+            data = data.to(self.device)
+
+            x = data.x
+            y = data.y.squeeze(-1).long()
+            pos = data.pos
+            batch = (
+                data.batch
+                if hasattr(data, "batch")
+                else torch.zeros(x.shape[0], dtype=torch.long, device=x.device)
+            )
+            object_ids = create_unique_object_ids(y, batch, noise_idx)
+
+            if downsample:
+                mask_scale = self.config.get("mask_scale", 1.0)
+                mask = create_sample_mask(
+                    object_ids,
+                    batch=batch,
+                    scale=mask_scale,
+                    bkg_id=noise_idx,
+                )
+                x, pos, batch, object_ids = apply_mask(mask, x, pos, batch, object_ids)
+
+            x_c, beta = self.model(x, pos, batch)
+            beta = beta.squeeze(-1)
+
+            l_attr, l_repul, l_coward, l_noise = self._compute_oc_losses(
+                x_c, beta, object_ids, batch
+            )
+            loss = l_attr + l_repul + l_coward + l_noise
+            loss.backward()
+
+            self.optimizer.step()
+            self.writer.set_step((epoch - 1) * len(self.dataloader) + batch_idx)
+
+            self.writer.add_scalar('loss', loss.item())
+            self.writer.add_scalar('l_attr', l_attr.item())
+            self.writer.add_scalar('l_repul', l_repul.item())
+            self.writer.add_scalar('l_coward', l_coward.item())
+            self.writer.add_scalar('l_noise', l_noise.item())
+
+            if batch_idx % 10 == 0:
+                self.writer.add_histogram("beta_train", beta, bins='auto')
+
+            total_loss += loss.item()
+
+            if batch_idx % 10 == 0:
+                self.logger.info(
+                    'Train Epoch: {} {} Loss: {:.6f}'.format(
+                        epoch, self._progress(batch_idx), loss.item()
+                    )
+                )
+
+        self.writer.add_scalar("total_loss", total_loss)
+        log = {"loss": total_loss}
+
+        if self.lr_scheduler is not None:
+            self.writer.add_scalar("lr", self.lr_scheduler.get_last_lr()[0])
+
+        if self.do_validation:
+            val_log = self._valid_epoch(epoch)
+            log.update(**{"val_" + k: v for k, v in val_log.items()})
+
+        if self.lr_scheduler is not None:
+            self.lr_scheduler.step()
+        return log
+
+    def _valid_epoch(self, epoch):
+        self.model.eval()
+        total_loss = 0.0
+
+        noise_idx = self.config.get("noise_idx", -1)
+        with torch.no_grad():
+            for batch_idx, data in enumerate(self.valid_dataloader):
+                data = data.to(self.device)
+
+                x = data.x
+                y = data.y.squeeze(-1).long()
+                pos = data.pos
+                batch = (
+                    data.batch
+                    if hasattr(data, "batch")
+                    else torch.zeros(x.shape[0], dtype=torch.long, device=x.device)
+                )
+                object_ids = create_unique_object_ids(y, batch, noise_idx)
+
+                x_c, beta = self.model(x, pos, batch)
+                beta = beta.squeeze(-1)
+
+                l_attr, l_repul, l_coward, l_noise = self._compute_oc_losses(
+                    x_c, beta, object_ids, batch
+                )
+                loss = l_attr + l_repul + l_coward + l_noise
+
+                self.writer.set_step(
+                    (epoch - 1) * len(self.valid_dataloader) + batch_idx, 'valid'
+                )
+                self.writer.add_scalar('loss', loss.item())
+                self.writer.add_scalar('l_attr', l_attr.item())
+                self.writer.add_scalar('l_repul', l_repul.item())
+                self.writer.add_scalar('l_coward', l_coward.item())
+                self.writer.add_scalar('l_noise', l_noise.item())
+
+                if batch_idx % 10 == 0:
+                    self.writer.add_histogram("beta_valid", beta, bins='auto')
+
+                total_loss += loss.item()
+
+        self.writer.add_scalar('total_loss', total_loss)
+
+        for name, p in self.model.named_parameters():
+            self.writer.add_histogram(name, p, bins='auto')
+
+        return {
+            "loss": total_loss,
+        }
+
+    def export_onnx(self, pth: str | pathlib.Path = "model_best.onnx"):
+        """
+        Export the model to ONNX format using a sample batch from the dataloader. The input tensors are prepared in the same way as during training, including packing into graph batches and unpacking features.
+
+        Parameters
+        ----------
+        pth : str or pathlib.Path
+            The path where the ONNX model will be saved.
+        """
+        self.model.eval()
+        data = next(iter(self.dataloader))
+        data = data.to(self.device)
+        pos = data.pos
+        x = data.x
+        y = data.y.squeeze(-1).long()
+        batch = (
+            data.batch
+            if hasattr(data, "batch")
+            else torch.zeros(x.shape[0], dtype=torch.long, device=x.device)
+        )
+
+        node_size = Dim("node_size", min=1)
+        dynamic_shapes = {
+            "x": {0: node_size},
+            "pos": {0: node_size},
+            "batch": {0: node_size},
+        }
+
+        artifacts_dir = self.checkpoint_dir / "onnx_artifacts"
+        artifacts_dir.mkdir(parents=True, exist_ok=True)
+
+        torch.onnx.export(
+            self.model,
+            (x, pos, batch),
+            str(pth),
+            dynamo=True,
+            dynamic_shapes=dynamic_shapes,
+            input_names=["x", "pos", "batch"],
+            verify=True,
+            report=True,
+            artifacts_dir=str(artifacts_dir),
+        )
